c6a3ddd0e1b111beb73e9c3d3b5e4918fd2e64e3
bigxigua <1336945839@qq.com>
2018-04-22 21:24:35 +0800

    push

--- app/actions/index.js
+++ app/actions/index.js
@@ -1,6 +1,6 @@
-import io from 'socket.io-client'
+import io from 'socket.io-client' 
 
-export const socket = io('http://localhost:3003', {
+export const socket = io('http://127.0.0.1:8080', {
 	'force new connection': true
 });
 
